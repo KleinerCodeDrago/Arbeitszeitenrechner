6eea3546b75effe17ca08fe5063ee500bc0ee5a8
KleinerCodeDrago <71352143+KleinerCodeDrago@users.noreply.github.com>
2022-06-02 20:28:00 +0200

    change names

--- Arbeitszeitenrechner/app/src/main/java/de/littledragon/arbeitszeitenrechner/ui/dashboard/DashboardFragment.kt
+++ Arbeitszeitenrechner/app/src/main/java/de/littledragon/arbeitszeitenrechner/ui/dashboard/DashboardFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import de.littledragon.arbeitszeitenrechner.databinding.FragmentDashboardBinding
+import de.littledragon.arbeitszeitenrechner.databinding.FragmentInoutBinding
 
 class DashboardFragment : Fragment() {
 
-    private var _binding: FragmentDashboardBinding? = null
+    private var _binding: FragmentInoutBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,7 +25,7 @@ class DashboardFragment : Fragment() {
         val dashboardViewModel =
                 ViewModelProvider(this).get(DashboardViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentInoutBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textDashboard
