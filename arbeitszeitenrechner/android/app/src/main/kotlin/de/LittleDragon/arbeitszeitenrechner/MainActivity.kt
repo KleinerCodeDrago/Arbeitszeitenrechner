e12d35278d39ec346b05c8f126ce3b8c309b9954
KleinerCodeDrago <71352143+KleinerCodeDrago@users.noreply.github.com>
2022-06-12 16:29:57 +0200

    new init auf linux

--- arbeitszeitenrechner/android/app/src/main/kotlin/de/LittleDragon/arbeitszeitenrechner/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package de.LittleDragon.arbeitszeitenrechner
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ arbeitszeitenrechner_flutter/android/app/src/main/kotlin/de/KleinerDrache/arbeitszeitenrechner_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package de.KleinerDrache.arbeitszeitenrechner_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
